c38fa6761f42fea0051fbd785e3b676055c54af6
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-06-01 18:44:29 +0300

    fixed method post

--- routs/postsRouter.js
+++ routs/postsRouter.js
@@ -1,5 +1,5 @@
+const crypto = require("crypto");
 const express = require("express");
-const { rawListeners } = require("../app");
 const router = express.Router();
 
 const posts = [
@@ -25,14 +25,17 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  const [post] = posts.filter((post) => {
-    post.id === req.params.id;
+  const index = posts.findIndex((post) => {
+    return post.id === Number(req.params.id);
   });
-
-  res.status(200).json({ status: "success", code: 200, post });
+  res.status(200).json({ status: "success", code: 200, post: posts[index] });
 });
 
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { topic, text } = req.body;
+  posts.push({ id: crypto.randomUUID(), topic, text });
+  res.status(200).json({ status: "success", code: 200 });
+});
 
 router.put("/:id", (req, res) => {});
 
