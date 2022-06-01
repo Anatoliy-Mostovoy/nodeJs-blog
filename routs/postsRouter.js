65dabda488049076674fadad9c4b88cacc114444
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-06-01 21:57:46 +0300

    make correct structure of project

--- routs/postsRouter.js
+++ routs/postsRouter.js
@@ -6,53 +6,18 @@ const {
   putValidation,
 } = require("../middleware/validation.js");
 
-let posts = [
-  {
-    id: 1,
-    topic: "test1",
-    text: "text test1",
-  },
-  {
-    id: 2,
-    topic: "test2",
-    text: "text test2",
-  },
-  {
-    id: 3,
-    topic: "test3",
-    text: "text test3",
-  },
-];
+const {
+  getPosts,
+  getPostById,
+  postPost,
+  putPost,
+  deletePost,
+} = require("../controllers/postsControllers.js");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ posts });
-});
-
-router.get("/:id", (req, res) => {
-  const index = posts.findIndex((post) => post.id === Number(req.params.id));
-  res.status(200).json({ status: "success", code: 200, post: posts[index] });
-});
-
-router.post("/", postValidation, (req, res) => {
-  const { topic, text } = req.body;
-  posts.push({ id: crypto.randomUUID(), topic, text });
-  res.status(200).json({ status: "success", code: 200 });
-});
-
-router.put("/:id", putValidation, (req, res) => {
-  const { topic, text } = req.body;
-  posts.map((post) => {
-    if (post.id === Number(req.params.id)) {
-      post.topic = topic;
-      post.text = text;
-    }
-  });
-  res.status(200).json({ status: "success", code: 200, posts });
-});
-
-router.delete("/:id", (req, res) => {
-  posts = posts.filter((post) => post.id !== Number(req.params.id));
-  res.status(200).json({ status: "success", code: 200, posts });
-});
+router.get("/", getPosts);
+router.get("/:id", getPostById);
+router.post("/", postValidation, postPost);
+router.put("/:id", putValidation, putPost);
+router.delete("/:id", deletePost);
 
 module.exports = router;
