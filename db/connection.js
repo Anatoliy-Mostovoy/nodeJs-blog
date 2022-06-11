5c6b5092ddc286215d836c68aff96e8f60bde8b1
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-06-11 22:50:45 +0300

    fixed connection on mongoose

--- db/connection.js
+++ db/connection.js
@@ -1,22 +1,37 @@
 require("dotenv").config();
-const { MongoClient } = require("mongodb");
-const collections = {};
+const mongoose = require("mongoose");
 
-const getCollection = () => {
-  console.log(collections);
-  return collections;
-};
+const MONGO_URL = process.env.MONGO_URL;
 
 const connectMongo = async () => {
-  const client = await MongoClient.connect(process.env.MONGO_URL, {
+  mongoose.connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-
-  const db = client.db("blog");
-  collections.Posts = db.collection("posts");
-
-  console.log("Database connected successful!");
 };
 
-module.exports = { connectMongo, getCollection };
+module.exports = { connectMongo };
+
+//TODO on vanilla MongoDB
+// require("dotenv").config();
+// const { MongoClient } = require("mongodb");
+// const collections = {};
+
+// const getCollection = () => {
+//   console.log(collections);
+//   return collections;
+// };
+
+// const connectMongo = async () => {
+//   const client = await MongoClient.connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+
+//   const db = client.db("blog");
+//   collections.Posts = db.collection("posts");
+
+//   console.log("Database connected successful!");
+// };
+
+// module.exports = { connectMongo, getCollection };
