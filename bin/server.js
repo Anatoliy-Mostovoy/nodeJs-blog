42d32a12d8aa5485719fa095ae9fc52094ee6965
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-06-11 13:51:04 +0300

    connect MongoDB

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,26 @@
 require("dotenv").config();
 const app = require("../app.js");
+const { MongoClient } = require("mongodb");
 
 const PORT = process.env.PORT || 8081;
 
-app.listen(PORT, (err, req, res) => {
-  if (err) {
-    console.log(`Sorry, some error ${err}`);
-  }
-  console.log(`Server was start on PORT ${PORT}`);
-});
+const start = async () => {
+  const client = await MongoClient.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = client.db("blog");
+  const Posts = db.collection("posts");
+  const posts = await Posts.find({}).toArray();
+  console.log(posts);
+
+  app.listen(PORT, (err, req, res) => {
+    if (err) {
+      console.log(`Sorry, some error ${err}`);
+    }
+    console.log(`Server was start on PORT ${PORT}`);
+  });
+};
+
+start();
