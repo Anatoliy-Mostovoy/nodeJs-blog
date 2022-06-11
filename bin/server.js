8fd41903fee93282bdb3e77f31cc65e4d34db906
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-06-11 20:33:35 +0300

    wrap try...catch server start

--- bin/server.js
+++ bin/server.js
@@ -5,14 +5,18 @@ const { connectMongo } = require("../db/connection.js");
 const PORT = process.env.PORT || 8081;
 
 const start = async () => {
-  await connectMongo();
+  try {
+    await connectMongo();
 
-  app.listen(PORT, (err, req, res) => {
-    if (err) {
-      console.log(`Sorry, some error ${err}`);
-    }
-    console.log(`Server was start on PORT ${PORT}`);
-  });
+    app.listen(PORT, (err, req, res) => {
+      if (err) {
+        console.log(`Sorry, some error ${err}`);
+      }
+      console.log(`Server was start on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Filled to start server with error: ${error.message}`);
+  }
 };
 
 start();
