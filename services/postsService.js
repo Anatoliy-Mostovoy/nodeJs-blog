4e0be08c3d46cded559e5f4b58a75f8a2c39ea60
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-06-12 08:58:32 +0300

    addService to work with db

---
+++ services/postsService.js
@@ -0,0 +1,13 @@
+const getPosts = () => {};
+const getPostById = () => {};
+const addPost = () => {};
+const updatePostById = () => {};
+const deletePostById = () => {};
+
+module.exports = {
+  getPosts,
+  getPostById,
+  addPost,
+  updatePostById,
+  deletePostById,
+};
