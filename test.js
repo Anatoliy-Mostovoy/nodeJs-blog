b5377f87422efc6ae3c07eeb24514d001074cbf7
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2022-06-19 22:40:05 +0300

    test

--- test.js
+++ test.js
@@ -7,8 +7,17 @@ console.log((true && true) || false); //true
 console.log((false && true) || false); //false
 console.log((false && true) || true); //true
 
-const arr = [-1, 10];
-console.log(arr.length);
-for (let i = arr[0]; i <= arr[arr.length - 1]; i++) {
-  console.log(i);
-}
+// const arr = [-1, 10];
+// console.log(arr.length);
+// for (let i = arr[0]; i <= arr[arr.length - 1]; i++) {
+//   console.log(i);
+// }
+
+const string = "q,w,f,h";
+//{ name: 'q' }
+
+const arr = string.split(",");
+const newArr = arr.reduce((final, current) => {
+  return { name: current };
+}, []);
+console.log(newArr);
